3f534cc83aa4965ddced5dd1bbfc614a43cc8dbf
Paul Fiterau <fiteraup@yahoo.com>
2023-07-03 15:46:28 +0200

    Update learner with separate config options for Mapper.

--- ssh-learner/src/learner/SshSulServerConfig.java
+++ ssh-learner/src/learner/SshSulServerConfig.java
@@ -1,12 +1,26 @@
 package learner;
 
+import com.beust.jcommander.ParametersDelegate;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.config.SulServerConfig;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.mapper.config.MapperConnectionConfig;
 
-public class SshSulServerConfig extends SulServerConfig  implements SshMapperHostProvider {
+public class SshSulServerConfig extends SulServerConfig  implements SshMapperConfigProvider {
 
+    @ParametersDelegate
+    private SshMapperConfig sshMapperConfig;
+
+    public SshSulServerConfig() {
+        super.setHost("");
+        this.sshMapperConfig = new SshMapperConfig();
+    }
+    
     @Override
     public void applyDelegate(MapperConnectionConfig config) {
+    }
+
+    @Override
+    public SshMapperConfig getSshMapperConfig() {
+        return sshMapperConfig;
     }
 
 }
