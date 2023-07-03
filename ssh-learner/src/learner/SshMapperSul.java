952811d6b16e7ae582c76a502d1674b9764fd87e
Paul Fiterau <fiteraup@yahoo.com>
2023-07-03 15:46:28 +0200

    Update learner with separate config options for Mapper.

--- ssh-learner/src/learner/SshMapperSul.java
+++ ssh-learner/src/learner/SshMapperSul.java
@@ -6,6 +6,7 @@ import java.net.UnknownHostException;
 
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.AbstractSul;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.config.SulConfig;
+import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.sulwrappers.ProcessHandler;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.mapper.abstractsymbols.AbstractInput;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.mapper.abstractsymbols.AbstractOutput;
 import com.github.protocolfuzzing.protocolstatefuzzer.utils.CleanupTasks;
@@ -13,16 +14,21 @@ import com.github.protocolfuzzing.protocolstatefuzzer.utils.CleanupTasks;
 public class SshMapperSul extends AbstractSul {
     private SocketMapperSul socketSul;
 
-    public <T extends SulConfig & SshMapperHostProvider> SshMapperSul(T sulConfig, CleanupTasks cleanupTasks) throws UnknownHostException, IOException {
+    public <T extends SulConfig & SshMapperConfigProvider> SshMapperSul(T sulConfig, CleanupTasks cleanupTasks) throws UnknownHostException, IOException {
         super(sulConfig, cleanupTasks);
-        String host = sulConfig.getHost();
-        String[] hostSplit = host.split("\\:");
-        if (hostSplit.length != 2) {
+        String mapperAddress = sulConfig.getSshMapperConfig().getMapperAddress();
+        String[] addressSplit = mapperAddress.split("\\:");
+        if (addressSplit.length != 2) {
             throw new MapperException("Invalid mapper host, expected hostAddress:hostPort");
         }
-        String hostAddress = hostSplit[0];
-        Integer hostPort = Integer.valueOf(hostSplit[1]);
-        Socket sock = new Socket(hostAddress, hostPort);
+        String mapperIpAddress = addressSplit[0];
+        Integer mapperPort = Integer.valueOf(addressSplit[1]);
+        if (sulConfig.getSshMapperConfig().getMapperCommand() != null) {
+            MapperProcessHandler handler = new MapperProcessHandler(sulConfig.getSshMapperConfig().getMapperCommand(), 
+                    sulConfig.getSshMapperConfig().getMapperStartWait());
+            handler.launchProcess();
+        }
+        Socket sock = new Socket(mapperIpAddress, mapperPort);
         cleanupTasks.submit(new Runnable() {
             @Override
             public void run() {
@@ -53,5 +59,13 @@ public class SshMapperSul extends AbstractSul {
     public AbstractOutput step(AbstractInput in) {
         String output = socketSul.sendInput(in.getName());
         return new SshOutput(output);
+    }
+    
+    private static class MapperProcessHandler extends ProcessHandler {
+
+        protected MapperProcessHandler(String command, long startWait) {
+            super(command, startWait);
+        }
+        
     }
 }
