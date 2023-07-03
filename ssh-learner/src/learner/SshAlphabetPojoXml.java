7882b2708e0745f969b354947aa2b8837e4d9327
Paul Fiterau <fiteraup@yahoo.com>
2023-07-03 15:52:40 +0200

    Clean files using spotless.

--- ssh-learner/src/learner/SshAlphabetPojoXml.java
+++ ssh-learner/src/learner/SshAlphabetPojoXml.java
@@ -1,42 +1,40 @@
 package learner;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.github.protocolfuzzing.protocolstatefuzzer.components.learner.alphabet.xml.AlphabetPojoXml;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.mapper.abstractsymbols.AbstractInput;
-
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlAttribute;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlElements;
 import jakarta.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @XmlRootElement(name = "alphabet")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class SshAlphabetPojoXml extends AlphabetPojoXml {
-    
+
     @XmlElements(value = {
             @XmlElement(type = SshInputPojoXml.class, name = "SshInput")
     })
     private List<SshInputPojoXml> xmlInputs;
-    
+
     public SshAlphabetPojoXml() {
         xmlInputs = new ArrayList<>();
     }
-    
-    
+
+
     public List<AbstractInput> getInputs() {
         return xmlInputs.stream().map(xmlInput -> new SshInput(xmlInput.name)).collect(Collectors.toList());
     }
-    
+
     public static class SshInputPojoXml {
         @XmlAttribute(name = "name", required = true)
         private String name;
-        
+
         public SshInputPojoXml( ) {
         }
     }
--- ssh-learner/src/learner/SshMapperSul.java
+++ ssh-learner/src/learner/SshMapperSul.java
@@ -1,15 +1,14 @@
 package learner;
 
-import java.io.IOException;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.AbstractSul;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.config.SulConfig;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.sulwrappers.ProcessHandler;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.mapper.abstractsymbols.AbstractInput;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.mapper.abstractsymbols.AbstractOutput;
 import com.github.protocolfuzzing.protocolstatefuzzer.utils.CleanupTasks;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class SshMapperSul extends AbstractSul {
     private SocketMapperSul socketSul;
@@ -24,7 +23,7 @@ public class SshMapperSul extends AbstractSul {
         String mapperIpAddress = addressSplit[0];
         Integer mapperPort = Integer.valueOf(addressSplit[1]);
         if (sulConfig.getSshMapperConfig().getMapperCommand() != null) {
-            MapperProcessHandler handler = new MapperProcessHandler(sulConfig.getSshMapperConfig().getMapperCommand(), 
+            MapperProcessHandler handler = new MapperProcessHandler(sulConfig.getSshMapperConfig().getMapperCommand(),
                     sulConfig.getSshMapperConfig().getMapperStartWait());
             handler.launchProcess();
         }
@@ -42,7 +41,7 @@ public class SshMapperSul extends AbstractSul {
                 }
             }
         });
-        
+
         socketSul = new SocketMapperSul(sock);
     }
 
@@ -60,12 +59,12 @@ public class SshMapperSul extends AbstractSul {
         String output = socketSul.sendInput(in.getName());
         return new SshOutput(output);
     }
-    
+
     private static class MapperProcessHandler extends ProcessHandler {
 
         protected MapperProcessHandler(String command, long startWait) {
             super(command, startWait);
         }
-        
+
     }
 }
