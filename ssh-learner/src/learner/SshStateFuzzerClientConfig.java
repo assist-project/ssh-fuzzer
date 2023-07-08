124dbe3bdbce514668c08ea337cef1b546e84e6e
Paul Fiterau <fiteraup@yahoo.com>
2023-07-08 18:25:12 +0200

    Update with proper use of ProtocolState-Fuzzer's new configs.

--- ssh-learner/src/learner/SshStateFuzzerClientConfig.java
+++ ssh-learner/src/learner/SshStateFuzzerClientConfig.java
@@ -1,11 +1,14 @@
 package learner;
 
+import com.github.protocolfuzzing.protocolstatefuzzer.components.learner.config.LearnerConfigStandard;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.config.SulClientConfig;
 import com.github.protocolfuzzing.protocolstatefuzzer.statefuzzer.core.config.StateFuzzerClientConfigStandard;
+import com.github.protocolfuzzing.protocolstatefuzzer.statefuzzer.testrunner.core.config.TestRunnerConfigStandard;
+import com.github.protocolfuzzing.protocolstatefuzzer.statefuzzer.testrunner.timingprobe.config.TimingProbeConfigEmpty;
 
 public class SshStateFuzzerClientConfig extends StateFuzzerClientConfigStandard {
 
     public SshStateFuzzerClientConfig(SulClientConfig sulClientConfig) {
-        super(sulClientConfig);
+    	super(new LearnerConfigStandard(), sulClientConfig, new TestRunnerConfigStandard(), new TimingProbeConfigEmpty());
     }
 }
--- ssh-learner/src/learner/SshStateFuzzerServerConfig.java
+++ ssh-learner/src/learner/SshStateFuzzerServerConfig.java
@@ -1,11 +1,14 @@
 package learner;
 
+import com.github.protocolfuzzing.protocolstatefuzzer.components.learner.config.LearnerConfigStandard;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.config.SulServerConfig;
 import com.github.protocolfuzzing.protocolstatefuzzer.statefuzzer.core.config.StateFuzzerServerConfigStandard;
+import com.github.protocolfuzzing.protocolstatefuzzer.statefuzzer.testrunner.core.config.TestRunnerConfigStandard;
+import com.github.protocolfuzzing.protocolstatefuzzer.statefuzzer.testrunner.timingprobe.config.TimingProbeConfigEmpty;
 
 public class SshStateFuzzerServerConfig extends StateFuzzerServerConfigStandard {
 
     public SshStateFuzzerServerConfig(SulServerConfig sulServerConfig) {
-        super(sulServerConfig);
+        super(new LearnerConfigStandard(), sulServerConfig, new TestRunnerConfigStandard(), new TimingProbeConfigEmpty());
     }
 }
