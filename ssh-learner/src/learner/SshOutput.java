b66992fd3ab957e9f37320863c9607861a3c664a
shasan101 <hasantaqvi5@gmail.com>
2025-02-12 01:07:55 +0100

    added types for generics and implemented the required override funcs

--- ssh-learner/src/learner/SshOutput.java
+++ ssh-learner/src/learner/SshOutput.java
@@ -2,8 +2,19 @@ package learner;
 
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.mapper.abstractsymbols.AbstractOutput;
 
-public class SshOutput extends AbstractOutput {
-    public SshOutput(String name) {
-        super(name);
-    }
+public class SshOutput extends AbstractOutput<SshOutput, String> {
+
+	public SshOutput(String name) {
+		super(name);
+	}
+
+	@Override
+	protected SshOutput buildOutput(String name) {
+		return new SshOutput(name);
+	}
+
+	@Override
+	protected SshOutput convertOutput() {
+		return new SshOutput(this.name);
+	}
 }
