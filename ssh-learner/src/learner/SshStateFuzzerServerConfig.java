2efe652679b4e20687c30fe2fbe6673e95c59e87
shasan101 <hasantaqvi5@gmail.com>
2025-02-12 01:09:33 +0100

    reformatting from the ide

--- ssh-learner/src/learner/SshStateFuzzerServerConfig.java
+++ ssh-learner/src/learner/SshStateFuzzerServerConfig.java
@@ -4,11 +4,11 @@ import com.github.protocolfuzzing.protocolstatefuzzer.components.learner.config.
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.config.SulServerConfig;
 import com.github.protocolfuzzing.protocolstatefuzzer.statefuzzer.core.config.StateFuzzerServerConfigStandard;
 import com.github.protocolfuzzing.protocolstatefuzzer.statefuzzer.testrunner.core.config.TestRunnerConfigStandard;
-import com.github.protocolfuzzing.protocolstatefuzzer.statefuzzer.testrunner.timingprobe.config.TimingProbeConfigEmpty;
+import com.github.protocolfuzzing.protocolstatefuzzer.statefuzzer.testrunner.timingprobe.config.TimingProbeConfigStandard;
 
 public class SshStateFuzzerServerConfig extends StateFuzzerServerConfigStandard {
 
     public SshStateFuzzerServerConfig(SulServerConfig sulServerConfig) {
-        super(new LearnerConfigStandard(), sulServerConfig, new TestRunnerConfigStandard(), new TimingProbeConfigEmpty());
+        super(new LearnerConfigStandard(), sulServerConfig, new TestRunnerConfigStandard(), new TimingProbeConfigStandard());
     }
 }
