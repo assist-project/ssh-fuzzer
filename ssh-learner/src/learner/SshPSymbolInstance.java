84689c779be439f00fb0eca2bdeecdda68f184cb
shasan101 <hasantaqvi5@gmail.com>
2025-02-25 04:38:16 +0100

    basic classes needed to create the RA machine type

---
+++ ssh-learner/src/learner/SshPSymbolInstance.java
@@ -0,0 +1,13 @@
+package learner;
+
+import de.learnlib.ralib.data.DataValue;
+import de.learnlib.ralib.words.PSymbolInstance;
+import de.learnlib.ralib.words.ParameterizedSymbol;
+
+public class SshPSymbolInstance extends PSymbolInstance {
+
+    public SshPSymbolInstance(ParameterizedSymbol baseSymbol, DataValue... parameterValues) {
+        super(baseSymbol, parameterValues);
+        // TODO Auto-generated constructor stub
+    }
+}
---
+++ ssh-learner/src/learner/SshParameterizedSymbol.java
@@ -0,0 +1,12 @@
+package learner;
+
+import de.learnlib.ralib.words.ParameterizedSymbol;
+import de.learnlib.ralib.data.DataType;
+
+public class SshParameterizedSymbol extends ParameterizedSymbol {
+    public SshParameterizedSymbol(String name, DataType... pTypes) {
+        super(name, pTypes);
+    }
+
+    // You can add SSH-specific methods or constants if needed.
+}
