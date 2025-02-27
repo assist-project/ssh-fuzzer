f66ba58f5ba37711a5a438aae701dde3d19d5722
shasan101 <hasantaqvi5@gmail.com>
2025-02-27 01:56:15 +0100

    fixed docker-compose file name. fixed the ssh-keys dir

--- experiments/scripts/make_ssh_keys.sh
+++ experiments/scripts/make_ssh_keys.sh
@@ -4,6 +4,7 @@ base_path="${curr_dir}/../.."
 pushd ${base_path}
 
 if [ -z "$( ls -A './ssh-keys' )" ]; then
+   mkdir ${PWD}/ssh-keys
    ssh-keygen -t rsa -f ${PWD}/ssh-keys/learner-ssh -N ""
 fi
 popd
